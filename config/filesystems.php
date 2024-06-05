4e7593513c087579fe205a7a30ee18ba152b12f4
OlgaStude <112942851+OlgaStude@users.noreply.github.com>
2024-06-05 20:04:43 +0400

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -70,8 +70,7 @@ return [
     */
 
     'links' => [
-        public_path('storage') => storage_path('app/public'),
-        base_path('public_html') => base_path('public'),
+        public_path('storage') => storage_path('app/public')
     ],
 
 ];
